574bc80ab737987b7d889939b0236a2039ae146f
Yana Tsishchykava <yanatisch.dev@gmail.com>
2024-11-07 15:05:37 +0300

    feat: solved task 8

--- src/sumInRange.js
+++ src/sumInRange.js
@@ -5,5 +5,12 @@
  * @returns {number}
  */
 module.exports.sumInRange = function sumInRange(start, end) {
-  throw new Error('Not implemented');
+  start = Number(start);
+  end = Number(end);
+  if (start > end) {
+    [start, end] = [end, start]; // Если start больше end, меняем их местами
+  }
+  let number = end - start + 1; 
+  let result = (number * (start + end)) / 2;
+    return result;
 };
