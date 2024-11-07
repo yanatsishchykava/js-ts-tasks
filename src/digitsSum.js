a41229439200f544fca627f26980723a3159a6a7
Yana Tsishchykava <yanatisch.dev@gmail.com>
2024-11-07 11:34:36 +0300

    feat: solved task 4

--- src/digitsSum.js
+++ src/digitsSum.js
@@ -4,5 +4,10 @@
  * @returns {number}
  */
 module.exports.digitsSum = function digitsSum(n) {
-  throw new Error('Not implemented'); // remove this line
+    let sum = 0;
+    let nStr = String(Math.abs(n));
+    for (let i = 0; i < nStr.length; i++) {
+      sum += Number(nStr[i]);
+    }
+    return sum;
 };
