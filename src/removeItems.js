f06db550b60d4c13b46f27a482f52745ba79429a
Yana Tsishchykava <yanatisch.dev@gmail.com>
2024-11-21 11:59:17 +0300

    feat: solved task 4

--- src/removeItems.js
+++ src/removeItems.js
@@ -5,5 +5,12 @@
  * @returns {Object}
  */
 module.exports.removeItems = function removeItems(arr, value) {
-  throw new Error('Not implemented'); // remove me and write a solution
+const valueString = JSON.stringify(value);
+const result = [];
+for (let i = 0; i < arr.length; i++) {
+  if (JSON.stringify(arr[i]) !== valueString) {
+    result.push(arr[i]);
+  }
+}
+return result;
 };
