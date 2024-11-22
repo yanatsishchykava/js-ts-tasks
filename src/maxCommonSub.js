959d34900c1b2a16842479bfc5e099f941f248f5
Yana Tsishchykava <yanatisch.dev@gmail.com>
2024-11-22 15:47:09 +0300

    feat: solved task 8

--- src/maxCommonSub.js
+++ src/maxCommonSub.js
@@ -9,5 +9,14 @@
  * @returns {number}
  */
 module.exports.maxCommonSub = function maxCommonSub(str1, str2) {
-  throw new Error('Not implemented'); // remove me and write a solution
+  let maxStr = ''; 
+  for (let i = 0; i < str1.length; i++) {
+    for (let j = i + 1; j <= str1.length; j++) {
+      let substring = str1.slice(i, j);
+      if (str2.includes(substring) && substring.length > maxStr.length) {
+        maxStr = substring;
+      }
+    }
+  }
+  return maxStr;
 };
