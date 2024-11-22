763d525d4f3aa4a51851a5ad3bdfa6a3e56592ea
Yana Tsishchykava <yanatisch.dev@gmail.com>
2024-11-22 10:33:05 +0300

    feat: solved task 7

--- src/diffElems.js
+++ src/diffElems.js
@@ -4,5 +4,6 @@
  * @returns {number}
  */
 module.exports.diffElems = function diffElems(arr) {
-  throw new Error('Not implemented'); // remove me and write a solution
+ let uniqueElements = new Set(arr);
+return uniqueElements.size
 };
