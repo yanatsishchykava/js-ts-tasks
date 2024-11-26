09e65eb4ed66116eb0068e231a5c31eda16fd5f6
Andrei_Kandratsenka <andrei_kandratsenka@epam.com>
2024-11-26 20:29:50 +0200

    feat: made test folder as a source to get tests list

--- test/index.js
+++ test/index.js
@@ -5,19 +5,20 @@ const fs = require('fs');
 
 const solutionsPath = path.join(__dirname, '../src');
 const testPath = __dirname;
+const tests = fs
+  .readdirSync(testPath)
+  .filter(v => v.endsWith('json'))
+  .map(v => v.replace('.json', ''));
 
-const solutions = fs.readdirSync(solutionsPath);
-
-solutions.forEach(solution => {
-  const testName = solution.replace(/\.js/, '');
+tests.forEach(testName => {
   // eslint-disable-next-line import/no-dynamic-require,global-require
-  const importedModule = require(`${solutionsPath}/${solution}`);
+  const importedModule = require(`${solutionsPath}/${testName}.js`);
   const method = importedModule[testName];
   // eslint-disable-next-line import/no-dynamic-require,global-require
   const testCases = require(`${testPath}/${testName}`);
 
   if (method) {
-    describe(solution, () => {
+    describe(testName, () => {
       testCases.forEach(testCase => {
         it(`should return "${JSON.stringify(testCase.expected)}"`, () => {
           const fn = method(...testCase.fnOuterArguments);
